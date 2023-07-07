262316de7d84e683ce3450d29baaffd7973b5577
unknown <sisiphopepengweni@gmail.com>
2023-07-07 16:43:13 +0200

    all commits

--- src/main.js
+++ src/main.js
@@ -6,5 +6,6 @@ import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import 'animate.css';
+import '@/components/css/style.css'
 
 createApp(App).use(store).use(router).mount('#app')
